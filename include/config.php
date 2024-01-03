14dddae4b52a8d879a652f496af651cb5a86818f
xmartina <martinsprince126@gmail.com>
2024-01-03 02:39:33 +0100

    i

---
+++ include/config.php
@@ -0,0 +1,33 @@
+<?php
+
+define("WEB_TITLE","South Monument Bank"); // Change Bank Name
+define("WEB_URL","https://dashboard.southmonumentplc.online"); // Change No "/" Ending splash
+define("WEB_EMAIL","contact@southmonumentplc.online"); // Change Your Website Email
+
+$web_url = WEB_URL;
+function support_plugin(){
+    require_once 'support_plugin.php';
+}
+
+function dbConnect(){
+    $servername = "localhost";
+    $username = "multistream6_south_monument_plc_1";//DATABASE USERNAME
+    $password = "000000";//DATABASE PASSWORD
+    $database = "multistream6_south_monument_plc_1";//DATABASE NAME
+    $dns = "mysql:host=$servername;dbname=$database";
+
+    try {
+        $conn = new PDO($dns, $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+//return dbConnect();
+
+function inputValidation($value): string
+{
+    return trim(htmlspecialchars(htmlentities($value)));
+}
